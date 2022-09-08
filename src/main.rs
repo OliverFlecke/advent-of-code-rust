43c1dc6322bde7125b5777b2b9083967737b9819
Oliver Fleckenstein <oliverfl@live.dk>
2022-09-08 23:18:51 +0100

    solve: Solutions for 2017 day 2

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,30 @@ use std::error::Error;
 
 mod solutions;
 
-use crate::solutions::{y2017::day01::Day01, Solution};
+use crate::solutions::Solution;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // TODO: Implement arg parser to get these values
     let year = Year::Y2017;
-    let day = 1;
+    let day = 2;
 
     let problem_input = match get_input(year, day) {
         Ok(input) => input,
         Err(_) => panic!("Unable to get input"),
     };
 
-    submit(Year::Y2017, 1, Level::A, &Day01::solve_a(&problem_input));
-    submit(Year::Y2017, 1, Level::B, &Day01::solve_b(&problem_input));
+    submit(
+        Year::Y2017,
+        day,
+        Level::A,
+        &solutions::y2017::day02::Day02::solve_a(&problem_input),
+    );
+    submit(
+        Year::Y2017,
+        day,
+        Level::B,
+        &solutions::y2017::day02::Day02::solve_b(&problem_input),
+    );
 
     Ok(())
 }
---
+++ src/solutions/y2017/day02.rs
@@ -0,0 +1,78 @@
+use itertools::{Itertools, MinMaxResult};
+
+use crate::solutions::Solution;
+
+pub struct Day02 {}
+
+impl Solution for Day02 {
+    fn solve_a<S>(input: S) -> String
+    where
+        S: AsRef<str>,
+    {
+        input
+            .as_ref()
+            .split('\n')
+            .filter(|line| !line.is_empty())
+            .map(|line| {
+                match line
+                    .split('\t')
+                    .filter(|n| !n.trim().is_empty())
+                    .map(|n| n.parse::<u64>().unwrap())
+                    .minmax()
+                {
+                    MinMaxResult::MinMax(min, max) => max - min,
+                    x => panic!("No min/max found: {:?}", x),
+                }
+            })
+            .sum::<u64>()
+            .to_string()
+    }
+
+    fn solve_b<S>(input: S) -> String
+    where
+        S: AsRef<str>,
+    {
+        input
+            .as_ref()
+            .split('\n')
+            .filter(|line| !line.is_empty())
+            .map(|line| {
+                let numbers = line
+                    .split('\t')
+                    .filter(|n| !n.trim().is_empty())
+                    .map(|n| n.parse::<u64>().unwrap())
+                    .sorted()
+                    .collect_vec();
+
+                for i in 0..numbers.len() {
+                    for j in i + 1..numbers.len() {
+                        let (a, b) = (numbers[j], numbers[i]);
+                        if a % b == 0 {
+                            return a / b;
+                        }
+                    }
+                }
+                unreachable!()
+            })
+            .sum::<u64>()
+            .to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_a() {
+        let input = "5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8";
+
+        assert_eq!(Day02::solve_a(input), "18");
+    }
+
+    #[test]
+    fn test_b() {
+        let input = "5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5";
+        assert_eq!(Day02::solve_b(input), "9");
+    }
+}
