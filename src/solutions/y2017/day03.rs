20d10aed4f039a4b6f89310ac7a57288acdd6d80
Oliver Fleckenstein <oliverfl@live.dk>
2022-09-09 13:02:37 +0100

    solve: Solutions for 2017 day 3 part B

--- src/solutions/y2017/day03.rs
+++ src/solutions/y2017/day03.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 use crate::solutions::{Answer, Solution};
@@ -30,8 +32,59 @@ impl Solution for Day03 {
             .into()
     }
 
-    fn solve_b(&self, _input: &str) -> Answer {
-        todo!()
+    fn solve_b(&self, input: &str) -> Answer {
+        fn sum_of_neighbors(a: i32, b: i32, grid: &HashMap<(i32, i32), u32>) -> u32 {
+            (-1..=1)
+                .map(|x| (-1..=1).map(move |y| (x, y)))
+                .flatten()
+                .filter(|p| *p != (0, 0))
+                .map(|(x, y)| grid.get(&(a + x, b + y)).unwrap_or(&0))
+                .sum::<u32>()
+        }
+
+        let target: u32 = input.parse().unwrap();
+
+        let mut grid: HashMap<(i32, i32), u32> = HashMap::new();
+        grid.insert((0, 0), 1);
+
+        let mut k = 1;
+        let (mut x, mut y) = (0, 0);
+        loop {
+            for _ in 0..k {
+                y += 1;
+                let value = sum_of_neighbors(x, y, &grid);
+                if value > target {
+                    return value.into();
+                }
+                grid.insert((x, y), value);
+            }
+            for _ in 0..k {
+                x -= 1;
+                let value = sum_of_neighbors(x, y, &grid);
+                if value > target {
+                    return value.into();
+                }
+                grid.insert((x, y), value);
+            }
+            k += 1;
+            for _ in 0..k {
+                y -= 1;
+                let value = sum_of_neighbors(x, y, &grid);
+                if value > target {
+                    return value.into();
+                }
+                grid.insert((x, y), value);
+            }
+            for _ in 0..k {
+                x += 1;
+                let value = sum_of_neighbors(x, y, &grid);
+                if value > target {
+                    return value.into();
+                }
+                grid.insert((x, y), value);
+            }
+            k += 1;
+        }
     }
 }
 
@@ -48,5 +101,10 @@ mod tests {
         assert_eq!(Day03 {}.solve_a("23"), Answer::UInt(2));
         assert_eq!(Day03 {}.solve_a("12"), Answer::UInt(3));
         assert_eq!(Day03 {}.solve_a("1024"), Answer::UInt(31));
+    }
+
+    #[test]
+    fn test_b() {
+        assert_eq!(Day03 {}.solve_b("121"), Answer::UInt(122))
     }
 }
