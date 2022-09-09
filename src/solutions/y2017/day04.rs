3baa5e2e23803a2fa899f003db7f66eff03b0499
Oliver Fleckenstein <oliverfl@live.dk>
2022-09-09 22:17:17 +0100

    solve: Solutions for 2017 day 4 part B

--- src/solutions/y2017/day04.rs
+++ src/solutions/y2017/day04.rs
@@ -1,4 +1,6 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, iter::FromIterator};
+
+use itertools::Itertools;
 
 use crate::solutions::{answer::Answer, Solution};
 
@@ -28,8 +30,29 @@ impl Solution for Day04 {
             .into()
     }
 
-    fn solve_b(&self, _input: &str) -> Answer {
-        todo!()
+    fn solve_b(&self, input: &str) -> Answer {
+        fn is_valid_passphrase(phrase: &str) -> bool {
+            let mut letter_sets = HashSet::new();
+
+            for word in phrase.split(' ') {
+                let letters = String::from_iter(word.chars().sorted());
+
+                if letter_sets.contains(&letters) {
+                    return false;
+                }
+
+                letter_sets.insert(letters);
+            }
+
+            true
+        }
+
+        input
+            .split('\n')
+            .filter(|phrase| !phrase.is_empty())
+            .filter(|phrase| is_valid_passphrase(&phrase))
+            .count()
+            .into()
     }
 }
 
@@ -42,5 +65,31 @@ mod tests {
         assert_eq!(Day04 {}.solve_a("aa bb cc dd ee"), Answer::UInt(1));
         assert_eq!(Day04 {}.solve_a("aa bb cc dd aa"), Answer::UInt(0));
         assert_eq!(Day04 {}.solve_a("aa bb cc dd aaa"), Answer::UInt(1));
+    }
+
+    #[test]
+    fn test_b() {
+        assert_eq!(Day04 {}.solve_b("abcde fghij"), Answer::UInt(1));
+        assert_eq!(Day04 {}.solve_b("abcde xyz ecdab"), Answer::UInt(0));
+        assert_eq!(Day04 {}.solve_b("a ab abc abd abf abj"), Answer::UInt(1));
+        assert_eq!(
+            Day04 {}.solve_b("iiii oiii ooii oooi oooo"),
+            Answer::UInt(1)
+        );
+        assert_eq!(Day04 {}.solve_b("oiii ioii iioi iiio"), Answer::UInt(0));
+    }
+
+    #[test]
+    fn hast_set_equality() {
+        let mut a = HashSet::new();
+        a.insert('a');
+        a.insert('b');
+        a.insert('c');
+        let mut b = HashSet::new();
+        b.insert('a');
+        b.insert('b');
+        b.insert('c');
+
+        assert_eq!(a, b);
     }
 }
