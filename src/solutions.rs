0740a08874c6d53886d9d13b7090bf6a66eb687b
Oliver Fleckenstein <oliverfl@live.dk>
2022-09-17 23:41:57 +0100

    solve: Solutions for 2017 day 9 part A

--- src/solutions.rs
+++ src/solutions.rs
@@ -21,6 +21,7 @@ pub fn get_solver(year: Year, day: Day) -> Box<dyn Solution> {
             6 => Box::new(y2017::day06::Day06 {}),
             7 => Box::new(y2017::day07::Day07 {}),
             8 => Box::new(y2017::day08::Day08 {}),
+            9 => Box::new(y2017::day09::Day09 {}),
             _ => panic!("Solution for day {} not found", day),
         },
         _ => panic!("Solution for year {:?} was not found", year),
---
+++ src/solutions/y2017/day09.rs
@@ -0,0 +1,76 @@
+use itertools::Itertools;
+
+use crate::solutions::{answer::Answer, Solution};
+
+pub struct Day09 {}
+
+impl Solution for Day09 {
+    fn solve_a(&self, input: &str) -> Answer {
+        // println!("Analyzing: {}", input);
+        let chars = input.chars().collect_vec();
+        let mut score: u64 = 0;
+        let mut depth: u64 = 0;
+        let mut i = 0;
+
+        while i < chars.len() {
+            match chars[i] {
+                '{' => {
+                    depth += 1;
+                }
+                '}' => {
+                    score += depth;
+                    depth -= 1;
+                }
+                '<' => {
+                    // print!("Found garbage: ");
+                    let mut next_negated = false;
+                    while i < chars.len() && (chars[i] != '>' || next_negated) {
+                        // print!("{}", chars[i]);
+                        if next_negated {
+                            next_negated = false;
+                        } else {
+                            next_negated = chars[i] == '!';
+                        }
+                        i += 1;
+                    }
+                    // println!("{}", chars[i]);
+                    // println!("{} or {}", chars[i] != '>', next_negated);
+                }
+                _ => {}
+            }
+            i += 1;
+        }
+
+        score.into()
+    }
+
+    fn solve_b(&self, _input: &str) -> Answer {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_a() {
+        assert_eq!(Day09 {}.solve_a("{}"), Answer::UInt(1));
+        assert_eq!(Day09 {}.solve_a("{{{}}}"), Answer::UInt(6));
+        assert_eq!(Day09 {}.solve_a("{{},{}}"), Answer::UInt(5));
+        assert_eq!(Day09 {}.solve_a("{{{},{},{{}}}}"), Answer::UInt(16));
+        assert_eq!(Day09 {}.solve_a("{<a>,<a>,<a>,<a>}"), Answer::UInt(1));
+        assert_eq!(
+            Day09 {}.solve_a("{{<ab>},{<ab>},{<ab>},{<ab>}}"),
+            Answer::UInt(9),
+        );
+        assert_eq!(
+            Day09 {}.solve_a("{{<!!>},{<!!>},{<!!>},{<!!>}}"),
+            Answer::UInt(9),
+        );
+        assert_eq!(
+            Day09 {}.solve_a("{{<a!>},{<a!>},{<a!>},{<ab>}}"),
+            Answer::UInt(3),
+        );
+    }
+}
