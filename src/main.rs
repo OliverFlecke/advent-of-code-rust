f9d42cb2f052d9548a41a8c58c0d7a139fd28f78
Oliver Fleckenstein <oliverfl@live.dk>
2022-09-19 17:51:37 +0100

    feat: getting the time for solutions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use advent_of_code::{client::*, Day, Level, Year};
 use solutions::get_solver;
-use std::{env, error::Error};
+use std::{env, error::Error, time::Instant};
 
 mod solutions;
 
@@ -13,40 +13,36 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     let solver = get_solver(year, day);
-    submit(
-        year,
-        day,
-        Level::A,
-        &solver.solve_a(&problem_input).to_string(),
-    );
-    submit(
-        year,
-        day,
-        Level::B,
-        &solver.solve_b(&problem_input).to_string(),
-    );
+    let start_a = Instant::now();
+    let answer_a = solver.solve_a(&problem_input);
+    println!("Part A: {:?}", start_a.elapsed());
+
+    submit(year, day, Level::A, &answer_a.to_string());
+
+    let start_b = Instant::now();
+    let answer_b = solver.solve_b(&problem_input);
+    println!("Part B: {:?}", start_b.elapsed());
+    submit(year, day, Level::B, &answer_b.to_string());
 
     Ok(())
 }
 
 fn parse_args() -> (Year, Day) {
     let args: Vec<String> = env::args().collect();
-    match args.len() {
-        3 => {
-            let year: Year = match args[1].parse::<u32>() {
-                Ok(n) => Year::from(n),
-                Err(_) => panic!("Cannot parse parameter as a valid year"),
-            };
-            let day: Day = match args[2].parse::<u8>() {
-                Ok(n) => match n {
-                    1..=25 => n,
-                    _ => panic!("Day must be a number from 1 to 25 (inclusive)"),
-                },
-                Err(_) => panic!("Connect parse 2nd parameter as a valid day"),
-            };
-
-            (year, day)
-        }
-        _ => panic!("Please pass the year and day to run"),
+    if args.len() < 3 {
+        panic!("Please pass the year and day to run");
     }
+    let year: Year = match args[1].parse::<u32>() {
+        Ok(n) => Year::from(n),
+        Err(_) => panic!("Cannot parse parameter as a valid year"),
+    };
+    let day: Day = match args[2].parse::<u8>() {
+        Ok(n) => match n {
+            1..=25 => n,
+            _ => panic!("Day must be a number from 1 to 25 (inclusive)"),
+        },
+        Err(_) => panic!("Connect parse 2nd parameter as a valid day"),
+    };
+
+    (year, day)
 }
