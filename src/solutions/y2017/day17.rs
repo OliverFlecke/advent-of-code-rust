bbe5898c1763b592acdefcf028c4daba225f1186
Oliver Fleckenstein <oliverfl@live.dk>
2022-09-23 15:12:25 +0200

    solve: Solutions for 2017 day 17 part B

--- src/solutions/y2017/day17.rs
+++ src/solutions/y2017/day17.rs
@@ -7,26 +7,49 @@ impl Solution for Day17 {
         let times = 2017;
         let amount: usize = input.parse().unwrap();
         let mut buffer = vec![0];
+        let mut index = 0;
 
         for size in 0..times {
-            buffer.rotate_left(amount % (size + 1));
-            buffer.push(size + 1);
+            index = (index + amount) % (size + 1) + 1;
+            buffer.insert(index, size + 1);
         }
 
-        buffer[0].into()
+        buffer[index + 1 % buffer.len()].into()
     }
 
-    fn solve_b(&self, _input: &str) -> Answer {
-        todo!()
+    fn solve_b(&self, input: &str) -> Answer {
+        let times = 50_000_000;
+        let amount: usize = input.parse().unwrap();
+        let mut next = 0;
+        let mut answer = 0;
+        for i in 1..times + 1 {
+            next = (next + amount) % i;
+            if next == 0 {
+                answer = i;
+            }
+            next += 1;
+        }
+
+        answer.into()
     }
 }
 
 #[cfg(test)]
 mod test {
+    use advent_of_code::client::get_input;
+
     use super::*;
 
     #[test]
     fn test_a() {
         assert_eq!(Day17 {}.solve_a("3"), Answer::UInt(638));
+    }
+
+    #[test]
+    fn test_b() {
+        assert_eq!(
+            Day17 {}.solve_b(get_input(advent_of_code::Year::Y2017, 17).unwrap().as_str()),
+            Answer::UInt(41797835)
+        );
     }
 }
