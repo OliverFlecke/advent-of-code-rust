f95dd2a8025074d1c609aebd7bb6bd5b8c60990b
Oliver Fleckenstein <oliverfl@live.dk>
2023-12-05 22:12:06 +0000

    refactor: Solution for 2023/04

--- src/solutions/y2023/day04.rs
+++ src/solutions/y2023/day04.rs
@@ -5,61 +5,53 @@ pub struct Day04;
 
 impl Solution for Day04 {
     fn solve_a(&self, input: &str) -> Option<Answer> {
-        fn to_set(s: &str) -> HashSet<usize> {
-            s.split_whitespace()
-                .map(|x| x.parse::<usize>().unwrap())
-                .collect()
-        }
-
         let answer = input
             .lines()
-            .map(|l| {
-                l.split_once(':')
-                    .map(|(_, numbers)| numbers)
-                    .and_then(|numbers| numbers.split_once('|'))
-                    .map(|(winners, mine)| (to_set(winners), to_set(mine)))
-                    .map(|(w, m)| w.intersection(&m).collect::<HashSet<_>>().len())
-                    .filter(|x| *x != 0)
-                    .map(|count| 2_usize.pow((count - 1) as u32))
-                    .unwrap_or_default()
-            })
+            .map(play_round)
+            .map(|count| 2_usize.pow((count - 1) as u32))
             .sum::<usize>();
 
         Some(answer.into())
     }
 
     fn solve_b(&self, input: &str) -> Option<Answer> {
-        fn to_set(s: &str) -> HashSet<usize> {
-            s.split_whitespace()
-                .map(|x| x.parse::<usize>().unwrap())
-                .collect()
-        }
-
         let lines: Vec<_> = input.lines().collect();
         let mut counter = vec![1; lines.len()];
 
-        lines.iter().enumerate().for_each(|(i, l)| {
-            let wins = l
-                .split_once(':')
-                .map(|(_, numbers)| numbers)
-                .and_then(|numbers| numbers.split_once('|'))
-                .map(|(winners, mine)| (to_set(winners), to_set(mine)))
-                .map(|(w, m)| w.intersection(&m).collect::<HashSet<_>>().len())
-                .filter(|x| *x != 0)
-                .unwrap_or_default();
-            let total_cards_of_this_type = *counter.get(i).unwrap();
+        lines
+            .iter()
+            .map(|l| play_round(l))
+            .enumerate()
+            .for_each(|(i, wins)| {
+                let total_cards_of_this_type = *counter.get(i).unwrap();
 
-            (i + 1..=i + wins)
-                .filter(|j| *j < lines.len())
-                .for_each(|j| {
-                    counter[j] += total_cards_of_this_type;
-                });
-        });
+                (i + 1..=i + wins)
+                    .filter(|j| *j < lines.len())
+                    .for_each(|j| {
+                        counter[j] += total_cards_of_this_type;
+                    });
+            });
 
         let answer: usize = counter.iter().sum();
 
         Some(answer.into())
     }
+}
+
+fn play_round(line: &str) -> usize {
+    line.split_once(':')
+        .map(|(_, numbers)| numbers)
+        .and_then(|numbers| numbers.split_once('|'))
+        .map(|(winners, mine)| (to_set(winners), to_set(mine)))
+        .map(|(w, m)| w.intersection(&m).collect::<HashSet<_>>().len())
+        .filter(|x| *x != 0)
+        .unwrap_or_default()
+}
+
+fn to_set(s: &str) -> HashSet<usize> {
+    s.split_whitespace()
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect()
 }
 
 #[cfg(test)]
