629ac0f505d2a3e579d1cd83c3a9dd9da9e262c6
Oliver Fleckenstein <oliverfl@live.dk>
2023-12-06 01:34:10 +0000

    refactor: Solution for 2023/03

--- src/solutions/y2023/day03.rs
+++ src/solutions/y2023/day03.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use itertools::Itertools;
 use regex::Regex;
 
@@ -7,50 +9,87 @@ pub struct Day03;
 
 impl Solution for Day03 {
     fn solve_a(&self, input: &str) -> Option<Answer> {
-        let re_numbers = Regex::new(r"\d+").unwrap();
-        let re_symbols = Regex::new(r"[^\w.]").unwrap();
-        let numbers = input
-            .trim()
-            .lines()
-            .enumerate()
-            .flat_map(|(y, l)| {
-                re_numbers
-                    .find_iter(l)
-                    .map(|m| (m.as_str(), y, m.start(), m.len()))
-                    .collect::<Vec<_>>()
-            })
-            .collect::<Vec<_>>();
-
-        let symbols: Vec<(usize, usize)> = input
-            .trim()
-            .lines()
-            .enumerate()
-            .flat_map(|(y, l)| {
-                re_symbols
-                    .find_iter(l)
-                    .map(|m| (y, m.start()))
-                    .collect::<Vec<_>>()
-            })
-            .collect();
-
-        let adjencent = |x: usize, y: usize, length: usize| -> Vec<(usize, usize)> {
-            (y.saturating_sub(1)..=y + 1)
-                .cartesian_product(x.saturating_sub(1)..=x + length)
-                .collect()
-        };
+        let numbers = find_numbers(input);
+        let symbols = find_symbols(input, r"[^\w.]");
 
         let answer = numbers
             .iter()
-            .filter(|x| adjencent(x.2, x.1, x.3).iter().any(|v| symbols.contains(v)))
-            .map(|x| x.0.parse::<usize>().unwrap())
+            .filter(|num| adjencent(num).iter().any(|v| symbols.contains(v)))
+            .map(|num| num.value)
             .sum::<usize>();
 
         Some(answer.into())
     }
 
-    fn solve_b(&self, _input: &str) -> Option<Answer> {
-        None
+    fn solve_b(&self, input: &str) -> Option<Answer> {
+        let symbols = find_symbols(input, r"\*");
+        let numbers = find_numbers(input);
+
+        let answer = symbols
+            .iter()
+            .filter_map(|s| {
+                let numbers = numbers
+                    .iter()
+                    .filter(|num| adjencent(num).iter().any(|v| v == s))
+                    .map(|num| num.value)
+                    .collect::<Vec<_>>();
+                if numbers.len() == 2 {
+                    Some(numbers[0] * numbers[1])
+                } else {
+                    None
+                }
+            })
+            .sum::<usize>();
+
+        Some(answer.into())
     }
+}
+
+fn find_symbols(input: &str, re: &str) -> HashSet<(usize, usize)> {
+    let re_symbols = Regex::new(re).unwrap();
+    input
+        .trim()
+        .lines()
+        .enumerate()
+        .flat_map(|(y, l)| {
+            re_symbols
+                .find_iter(l)
+                .map(|m| (y, m.start()))
+                .collect::<Vec<_>>()
+        })
+        .collect()
+}
+
+struct Number {
+    value: usize,
+    y: usize,
+    x: usize,
+    length: usize,
+}
+
+fn find_numbers(input: &str) -> Vec<Number> {
+    let re_numbers = Regex::new(r"\d+").unwrap();
+    input
+        .trim()
+        .lines()
+        .enumerate()
+        .flat_map(|(y, l)| {
+            re_numbers
+                .find_iter(l)
+                .map(|m| Number {
+                    value: m.as_str().parse::<usize>().unwrap(),
+                    y,
+                    x: m.start(),
+                    length: m.len(),
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect()
+}
+fn adjencent(number: &Number) -> Vec<(usize, usize)> {
+    (number.y.saturating_sub(1)..=number.y + 1)
+        .cartesian_product(number.x.saturating_sub(1)..=number.x + number.length)
+        .collect()
 }
 
 #[cfg(test)]
@@ -58,10 +97,7 @@ mod test {
     use crate::{client::get_input, Year};
 
     use super::*;
-
-    #[test]
-    fn test_a() {
-        let input = r#"467..114..
+    const INPUT: &str = r#"467..114..
 ...*......
 ..35..633.
 ......#...
@@ -72,12 +108,28 @@ mod test {
 ...$.*....
 .664.598.."#;
 
-        assert_eq!(Day03 {}.solve_a(input), Some(Answer::UInt(4361)))
+    #[test]
+    fn test_a() {
+        assert_eq!(Day03 {}.solve_a(INPUT), Some(Answer::UInt(4361)))
+    }
+
+    #[test]
+    fn test_b() {
+        assert_eq!(Day03 {}.solve_b(INPUT), Some(Answer::UInt(467835)))
     }
 
     #[test]
     fn answer_a() {
         let input = get_input(Year::Y2023, 3).unwrap();
-        assert_eq!(Day03 {}.solve_a(input.as_str()), Some(Answer::UInt(509115)))
+        assert_eq!(Day03 {}.solve_a(input.as_str()), Some(Answer::UInt(509115)));
+    }
+
+    #[test]
+    fn answer_b() {
+        let input = get_input(Year::Y2023, 3).unwrap();
+        assert_eq!(
+            Day03 {}.solve_b(input.as_str()),
+            Some(Answer::UInt(75220503))
+        );
     }
 }
