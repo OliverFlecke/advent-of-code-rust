d08837a3c6c1ec3efdc174a5943053d8e828fcdf
Oliver Fleckenstein <oliverfl@live.dk>
2023-12-07 18:45:18 +0000

    refactor: Reuse functions between parts for 2023/07

--- src/solutions/y2023/day07.rs
+++ src/solutions/y2023/day07.rs
@@ -30,15 +30,13 @@ impl Solution for Day07 {
             .trim()
             .lines()
             .filter_map(|line| line.split_once(' '))
-            .map(|(hand, bid)| Card {
-                hand: hand
-                    .chars()
-                    // Bit of a hack. To reuse the same `Ord` implementation below,
-                    // we will just convert J into a different symbol.
-                    .map(|c| if c == 'J' { 'X' } else { c })
-                    .collect(),
-                kind: from_str_with_joker(hand),
-                bid: bid.parse::<usize>().unwrap(),
+            .map(|(hand, bid)| {
+                let hand = hand.replace('J', "X");
+                Card {
+                    kind: hand.as_str().into(),
+                    hand,
+                    bid: bid.parse::<usize>().unwrap(),
+                }
             })
             .sorted_by(Ord::cmp)
             .enumerate()
@@ -140,53 +138,31 @@ impl From<&str> for CardType {
             map
         });
 
-        let max = *map.values().max().unwrap_or(&0);
+        let jokers = *map.get(&'X').unwrap_or(&0);
+        let max = *map
+            .iter()
+            .filter(|(key, _)| **key != 'X')
+            .map(|(_, value)| value)
+            .max()
+            .unwrap_or(&0);
+
+        let two_pairs = map.values().filter(|x| **x == 2).count() == 2;
 
         use CardType::*;
-        match max {
-            5 => FiveOfAKind,
-            4 => FourOfAKind,
-            3 if map.values().any(|x| *x == 2) => FullHouse,
-            3 => ThreeOfAKind,
-            2 if map.values().filter(|x| **x == 2).count() == 2 => TwoPair,
-            2 => OnePair,
-            1 => HighCard,
-
-            _ => unreachable!(),
+        match (max, jokers) {
+            (5, _) | (4, 1) | (3, 2) | (2, 3) | (1, 4) | (0, 5) => FiveOfAKind,
+            (4, 0) | (3, 1) | (2, 2) | (1, 3) | (0, 4) => FourOfAKind,
+            (3, 0) if map.values().any(|x| *x == 2) => FullHouse,
+            (3, 0) => ThreeOfAKind,
+            (2, 1) if two_pairs => FullHouse,
+            (2, 1) => ThreeOfAKind,
+            (2, 0) if two_pairs => TwoPair,
+            (2, 0) => OnePair,
+            (1, 2) => ThreeOfAKind,
+            (1, 1) => OnePair,
+            (1, 0) => HighCard,
+            _ => unreachable!("Got {max}, {jokers}"),
         }
-    }
-}
-
-fn from_str_with_joker(value: &str) -> CardType {
-    let map = value.chars().fold(HashMap::new(), |mut map, c| {
-        *map.entry(c).or_insert(0) += 1;
-        map
-    });
-
-    let jokers = *map.get(&'J').unwrap_or(&0);
-    let max = *map
-        .iter()
-        .filter(|(key, _)| **key != 'J')
-        .map(|(_, value)| value)
-        .max()
-        .unwrap_or(&0);
-
-    let two_pairs = map.values().filter(|x| **x == 2).count() == 2;
-
-    use CardType::*;
-    match (max, jokers) {
-        (5, _) | (4, 1) | (3, 2) | (2, 3) | (1, 4) | (0, 5) => FiveOfAKind,
-        (4, 0) | (3, 1) | (2, 2) | (1, 3) | (0, 4) => FourOfAKind,
-        (3, 0) if map.values().any(|x| *x == 2) => FullHouse,
-        (3, 0) => ThreeOfAKind,
-        (2, 1) if two_pairs => FullHouse,
-        (2, 1) => ThreeOfAKind,
-        (2, 0) if two_pairs => TwoPair,
-        (2, 0) => OnePair,
-        (1, 2) => ThreeOfAKind,
-        (1, 1) => OnePair,
-        (1, 0) => HighCard,
-        _ => unreachable!("Got {max}, {jokers}"),
     }
 }
 
