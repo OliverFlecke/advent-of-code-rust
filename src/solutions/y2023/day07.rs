516dd583ce71debfe51bfc7ebb0c8b66c751cca9
Oliver Fleckenstein <oliverfl@live.dk>
2023-12-07 17:32:08 +0000

    feat: Solution for 2023/07 part B

--- src/solutions/y2023/day07.rs
+++ src/solutions/y2023/day07.rs
@@ -13,7 +13,7 @@ impl Solution for Day07 {
             .lines()
             .filter_map(|line| line.split_once(' '))
             .map(|(hand, bid)| Card {
-                hand,
+                hand: hand.to_string(),
                 kind: hand.into(),
                 bid: bid.parse::<usize>().unwrap(),
             })
@@ -25,19 +25,42 @@ impl Solution for Day07 {
         Some(answer.into())
     }
 
-    fn solve_b(&self, _input: &str) -> Option<Answer> {
-        None
+    fn solve_b(&self, input: &str) -> Option<Answer> {
+        let answer = input
+            .trim()
+            .lines()
+            .filter_map(|line| line.split_once(' '))
+            .map(|(hand, bid)| Card {
+                hand: hand
+                    .chars()
+                    .map(|c| if c == 'J' { 'X' } else { c })
+                    .collect(),
+                kind: from_str_with_joker(hand),
+                bid: bid.parse::<usize>().unwrap(),
+            })
+            .sorted_by(Ord::cmp)
+            .enumerate()
+            .map(|(rank, card)| (rank + 1) * card.bid)
+            .sum::<usize>();
+
+        Some(answer.into())
     }
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd)]
-struct Card<'a> {
-    hand: &'a str,
+#[derive(Debug, PartialEq, Eq)]
+struct Card {
+    hand: String,
     kind: CardType,
     bid: usize,
 }
 
-impl<'a> Ord for Card<'a> {
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Card {
     fn cmp(&self, other: &Self) -> Ordering {
         self.kind
             .partial_cmp(&other.kind)
@@ -57,6 +80,7 @@ impl<'a> Ord for Card<'a> {
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Value {
+    Joker,
     Two,
     Three,
     Four,
@@ -77,6 +101,7 @@ impl From<char> for Value {
         use Value::*;
 
         match value {
+            'X' => Joker,
             '2' => Two,
             '3' => Three,
             '4' => Four,
@@ -113,7 +138,7 @@ impl From<&str> for CardType {
             map
         });
 
-        let max = map.values().max().unwrap();
+        let max = *map.values().max().unwrap_or(&0);
 
         match max {
             5 => CardType::FiveOfAKind,
@@ -139,9 +164,42 @@ impl From<&str> for CardType {
     }
 }
 
+fn from_str_with_joker(value: &str) -> CardType {
+    let map = value.chars().fold(HashMap::new(), |mut map, c| {
+        *map.entry(c).or_insert(0) += 1;
+        map
+    });
+
+    let jokers = *map.get(&'J').unwrap_or(&0);
+    let max = *map
+        .iter()
+        .filter(|(key, _)| **key != 'J')
+        .map(|(_, value)| value)
+        .max()
+        .unwrap_or(&0);
+
+    let two_pairs = map.values().filter(|x| **x == 2).count() == 2;
+
+    use CardType::*;
+    match (max, jokers) {
+        (5, _) | (4, 1) | (3, 2) | (2, 3) | (1, 4) | (0, 5) => FiveOfAKind,
+        (4, 0) | (3, 1) | (2, 2) | (1, 3) | (0, 4) => FourOfAKind,
+        (3, 0) if map.values().any(|x| *x == 2) => FullHouse,
+        (3, 0) => ThreeOfAKind,
+        (2, 1) if two_pairs => FullHouse,
+        (2, 1) => ThreeOfAKind,
+        (2, 0) if two_pairs => TwoPair,
+        (2, 0) => OnePair,
+        (1, 2) => ThreeOfAKind,
+        (1, 1) => OnePair,
+        (1, 0) => HighCard,
+        _ => unreachable!("Got {max}, {jokers}"),
+    }
+}
+
 #[cfg(test)]
 mod test {
-    // use crate::{client::get_input, Year};
+    use crate::{client::get_input, Year};
 
     use super::*;
 
@@ -157,16 +215,16 @@ QQQJA 483
         assert_eq!(Day07 {}.solve_a(INPUT), Some(Answer::UInt(6440)));
     }
 
-    // #[test]
-    // fn solve_a() {
-    //     let input = get_input(Year::Y2023, 7).unwrap();
-    //     assert_eq!(Day07 {}.solve_a(&input), Some(Answer::UInt(todo!())));
-    // }
+    #[test]
+    fn solve_a() {
+        let input = get_input(Year::Y2023, 7).unwrap();
+        assert_eq!(Day07 {}.solve_a(&input), Some(Answer::UInt(249748283)));
+    }
 
-    // #[test]
-    // fn test_b() {
-    //     assert_eq!(Day07 {}.solve_b(INPUT), Some(Answer::UInt(todo!())));
-    // }
+    #[test]
+    fn test_b() {
+        assert_eq!(Day07 {}.solve_b(INPUT), Some(Answer::UInt(5905)));
+    }
 
     // #[test]
     // fn solve_b() {
