ccb405d4b19031496358ef208a166f73df7683c0
Oliver Fleckenstein <oliverfl@live.dk>
2023-12-14 22:34:32 +0000

    feat: Solution for 2023/13 part b

--- src/solutions/y2023/day13.rs
+++ src/solutions/y2023/day13.rs
@@ -14,9 +14,9 @@ impl Solution for Day13 {
                 Array2D::from_rows(&grid).unwrap()
             })
             .map(|image| {
-                vertical_split(&image)
+                vertical_split(&image, 0)
                     .map(|x| x + 1)
-                    .or(horizontal_split(&image).map(|x| (x + 1) * 100))
+                    .or(horizontal_split(&image, 0).map(|x| (x + 1) * 100))
                     .unwrap()
             })
             .sum();
@@ -25,20 +25,38 @@ impl Solution for Day13 {
     }
 
     fn solve_b(&self, input: &str) -> Option<Answer> {
-        None
+        let answer: usize = input
+            .trim()
+            .split("\n\n")
+            .map(|image| {
+                let grid: Vec<Vec<_>> = image.lines().map(|line| line.chars().collect()).collect();
+                Array2D::from_rows(&grid).unwrap()
+            })
+            .map(|image| {
+                vertical_split(&image, 1)
+                    .map(|x| x + 1)
+                    .or(horizontal_split(&image, 1).map(|x| (x + 1) * 100))
+                    .unwrap()
+            })
+            .sum();
+
+        Some(answer.into())
     }
 }
 
-fn vertical_split(image: &Array2D<char>) -> Option<usize> {
-    fn check_if_mirror(image: &Array2D<char>, mid: usize) -> bool {
+type Image = Array2D<char>;
+
+fn vertical_split(image: &Image, expected: usize) -> Option<usize> {
+    fn check_if_mirror(image: &Image, mid: usize, expected: usize) -> bool {
+        let mut diff = 0;
         let mut left = mid;
         let mut right = mid + 1;
         while let Ok(l) = image.column_iter(left)
             && let Ok(r) = image.column_iter(right)
         {
-            println!("Checking {left} vs {right}");
-            if !l.eq(r) {
-                println!("{left} <> {right}");
+            let sum = l.zip(r).filter(|(a, b)| a != b).count();
+            diff += sum;
+            if diff > expected {
                 return false;
             }
 
@@ -46,22 +64,23 @@ fn vertical_split(image: &Array2D<char>) -> Option<usize> {
             right += 1;
         }
 
-        true
+        diff == expected
     }
 
-    (0..image.num_columns() - 1).find(|mid| check_if_mirror(image, *mid))
+    (0..image.num_columns() - 1).find(|mid| check_if_mirror(image, *mid, expected))
 }
 
-fn horizontal_split(image: &Array2D<char>) -> Option<usize> {
-    fn check_if_mirror(image: &Array2D<char>, mid: usize) -> bool {
+fn horizontal_split(image: &Image, expected: usize) -> Option<usize> {
+    fn check_if_mirror(image: &Image, mid: usize, expected: usize) -> bool {
+        let mut diff = 0;
         let mut left = mid;
         let mut right = mid + 1;
         while let Ok(l) = image.row_iter(left)
             && let Ok(r) = image.row_iter(right)
         {
-            println!("Checking {left} vs {right}");
-            if !l.eq(r) {
-                println!("{left} <> {right}");
+            let sum = l.zip(r).filter(|(a, b)| a != b).count();
+            diff += sum;
+            if diff > expected {
                 return false;
             }
 
@@ -69,10 +88,10 @@ fn horizontal_split(image: &Array2D<char>) -> Option<usize> {
             right += 1;
         }
 
-        true
+        diff == expected
     }
 
-    (0..image.num_rows() - 1).find(|mid| check_if_mirror(image, *mid))
+    (0..image.num_rows() - 1).find(|mid| check_if_mirror(image, *mid, expected))
 }
 
 #[cfg(test)]
@@ -110,10 +129,10 @@ mod test {
         assert_eq!(Day13 {}.solve_a(&input), Some(Answer::UInt(37113)));
     }
 
-    // #[test]
-    // fn test_b() {
-    //     assert_eq!(Day13 {}.solve_b(INPUT), Some(Answer::UInt(todo!())));
-    // }
+    #[test]
+    fn test_b() {
+        assert_eq!(Day13 {}.solve_b(INPUT), Some(Answer::UInt(400)));
+    }
 
     // #[test]
     // fn solve_b() {
